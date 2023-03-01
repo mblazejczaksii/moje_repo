1a9ef666755b8edadc2f9f047cb874e65bc670a0
Maciej Blazejczak <mblazejczak@sii.pl>
2023-03-01 10:38:54 +0100

    - modify caesar_cipher function to avoid index out of range error,

--- caesar_cipher/cc_functions.py
+++ caesar_cipher/cc_functions.py
@@ -5,12 +5,16 @@ def caesar_cipher(word, shift, encode):
     new_word = ''
     for letter in word:
         number = letters.index(letter)
-        if number + shift > len(letters):
-            result = number + shift - len(letters)
-            letter = letters[result]
-        else:
-            if encode:
+        if encode:
+            if number + shift > len(letters):
+                result = (number + shift) % len(letters)
+                letter = letters[result]
+            else:
                 letter = letters[number + shift]
+        else:
+            if number - shift < len(letters):
+                result = (number - shift) % len(letters)
+                letter = letters[result]
             else:
                 letter = letters[number - shift]
         new_word = new_word + letter
