dc9fb8a4fc9642c92ecc389abef4d7e53d62ebea
Maciej Blazejczak <mblazejczak@sii.pl>
2023-03-08 17:19:41 +0100

    - almost finish ekstraklasa game

--- ekstraklasa_game/another_ekstraklasa.py
+++ ekstraklasa_game/another_ekstraklasa.py
@@ -1,79 +1,43 @@
 # generate pairs of games between all ekstraklasa teams (season 2022/23)
 # Maciej Błażejczak MBQA
-import random
+from tabulate import tabulate
+from eg_functions import generate_games, generate_score, count_points, create_final_table, create_losers_table
 
-
-def generate_games():
-    """
-    Function to generate pairs of games between teams
-    :return:
-        list: games - list of games between teams
-    """
-    teams = ["Rakow Czestochowa", "Legia Warszawa", "Lech Poznan", "Widzew Lodz", "Pogon Szczecin", "Cracovia Krakow",
-             "Warta Poznan", "Radomiak Radom", "Stal Mielec", "Slask Wroclaw", "Zaglebie Lubin", "Wisla Plock",
-             "Piast Gliwice", "Jagiellonia Bialystok", "Gornik Zabrze", "Korona Kielce", "Lechia Gdansk",
-             "Miedz Legnica"]
-    counter = len(teams)
-    all_games = []
-    game_schedule = []
-    while counter > 0:
-        my_team = teams[counter - 1]
-        for team in teams:
-            if team != my_team:
-                game_schedule.append([my_team, team])
-                game_schedule.append([team, my_team])
-        all_games.append(game_schedule)
-        counter -= 1
-        # remove duplicates from all_games
-    temp_game_container = []
-    games = []
-    for game in all_games:
-        if isinstance(game, list):
-            for n in game:
-                temp_game_container.append(n)
-    for game in temp_game_container:
-        if game not in games:
-            games.append(game)
-
-    return games
-
-
-def generate_score():
-    """
-    Function to generate game score
-    :return:
-        int:
-    """
-    team1 = random.randint(0, 3)
-    team2 = random.randint(0, 3)
-    return team1, team2
-
-
+# generate all possible games between teams
 games = generate_games()
 
+# generate score for all games
 results = []
 for game in games:
     team1, team2 = generate_score()
     results.append([game, team1, team2])
-print(results)
 
-
-# results = [[['Miedz Legnica', 'Rakow Czestochowa'], 0, 3], [['Rakow Czestochowa', 'Miedz Legnica'], 1, 1]]
-winner = []
-looser = []
-draw = []
-
+# assign results to teams
+winners = []
+losers = []
+draws = []
 for n in results:
     if n[1] > n[2]:
-        winner.append(n[0][0])
-        looser.append(n[0][1])
+        winners.append(n[0][0])
+        losers.append(n[0][1])
     elif n[1] < n[2]:
-        winner.append(n[0][1])
-        looser.append(n[0][0])
+        winners.append(n[0][1])
+        losers.append(n[0][0])
     else:
-        draw.append(n[0][0])
-        draw.append(n[0][1])
+        draws.append(n[0][0])
+        draws.append(n[0][1])
 
-print("Winner: ", winner)
-print("Looser: ", looser)
-print("Draw: ", draw)
+# counting points and create final table
+winners = count_points(list=winners, point=3)
+draws = count_points(list=draws, point=1)
+losers = count_points(list=losers, point=1)
+final_table_list = create_final_table(winners=winners, draws=draws)
+losers_table = create_losers_table(losers=losers)
+
+# define header names and display final table
+col_names = ["Team", "Points", "Decision"]
+print(tabulate(final_table_list, headers=col_names))
+
+# define header names and display losers table
+col_names = ["Team", "Looses", "Decision"]
+print(tabulate(losers_table, headers=col_names))
---
+++ ekstraklasa_game/eg_functions.py
@@ -0,0 +1,111 @@
+# functions fo ekstraklasa_game
+# Maciej Błażejczak MBQA
+import random
+
+
+def generate_games():
+    """
+    Function to generate pairs of games between teams
+    :return:
+        list: games - list of games between teams
+    """
+    teams = ["Rakow Czestochowa", "Legia Warszawa", "Lech Poznan", "Widzew Lodz", "Pogon Szczecin", "Cracovia Krakow",
+             "Warta Poznan", "Radomiak Radom", "Stal Mielec", "Slask Wroclaw", "Zaglebie Lubin", "Wisla Plock",
+             "Piast Gliwice", "Jagiellonia Bialystok", "Gornik Zabrze", "Korona Kielce", "Lechia Gdansk",
+             "Miedz Legnica"]
+    counter = len(teams)
+    all_games = []
+    game_schedule = []
+    while counter > 0:
+        my_team = teams[counter - 1]
+        for team in teams:
+            if team != my_team:
+                game_schedule.append([my_team, team])
+                game_schedule.append([team, my_team])
+        all_games.append(game_schedule)
+        counter -= 1
+        # remove duplicates from all_games
+    temp_game_container = []
+    games = []
+    for game in all_games:
+        if isinstance(game, list):
+            for n in game:
+                temp_game_container.append(n)
+    for game in temp_game_container:
+        if game not in games:
+            games.append(game)
+
+    return games
+
+
+def generate_score():
+    """
+    Function to generate game score
+    :return:
+        int: score for team1
+        int: score for team2
+    """
+    team1 = random.randint(0, 3)
+    team2 = random.randint(0, 3)
+    return team1, team2
+
+
+def count_points(list, point):
+    """
+    Function to calculate points
+    :param list:
+    :param point:
+    :return:
+    """
+    result = {}
+    for item in list:
+        key = item
+        value = list.count(item) * point
+        result.update({key: value})
+
+    result = dict(sorted(result.items(), key=lambda x: x[1], reverse=True))
+
+    return result
+
+
+def create_final_table(winners, draws):
+    """
+    Function to draw a final table
+    :param winners:
+    :param draws:
+    :return:
+    """
+    final_table_list = []
+
+    for key, value in winners.items():
+        if key in draws:
+            key = key
+            value = winners[key] + draws[key]
+            final_table_list.append([key, value])
+    final_table_list = sorted(final_table_list, key=lambda x: x[1], reverse=True)
+    champion = ['👑 WINNER 👑']
+    relegation = ['RELEGATION']
+
+    final_table_list[0] = final_table_list[0] + champion
+    final_table_list[-1] = final_table_list[-1] + relegation
+    final_table_list[-2] = final_table_list[-2] + relegation
+    final_table_list[-3] = final_table_list[-3] + relegation
+
+    return final_table_list
+
+
+def create_losers_table(losers):
+    """
+    Function to draw a final table
+    :param losers:
+    :return:
+    """
+    losers_table = []
+
+    for key, value in losers.items():
+        losers_table.append([key, value])
+    losers_table = sorted(losers_table, key=lambda x: x[1], reverse=True)
+    looser = ['THE BIGGEST LOOSER']
+    losers_table[0] = losers_table[0] + looser
+
+    return losers_table
